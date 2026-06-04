7175f0a4de38adfb1dd0cf56577d1cdf29f045f8
MoLei314 <ken5409217@gmail.com>
2026-06-04 18:16:01 +0800

    first

--- index.js
+++ index.js
@@ -33,131 +33,133 @@ app.post('/callback', line.middleware(config), (req, res) => {
 });
 
 // event handler
+// ==========================================
+// 核心事件處理器
+// ==========================================
 function handleEvent(event) {
-  if (event.type === 'follow') {
-    // 建立一個 Promise 來讓系統等待
-    return new Promise((resolve) => {
-      
-      // setTimeout 控制延遲時間：2000 毫秒 = 2 秒
-      setTimeout(() => {
-        const replyMessage = {
-          type: 'text',
-          text: '👇 請點擊下方選單獲取更多資訊500', 
-          quickReply: quickReplyItems
-        };
-        
-        // 2 秒後呼叫 LINE API 傳送按鈕
-        client.replyMessage(event.replyToken, replyMessage)
-          .then(() => resolve(null))
-          .catch((err) => {
-            console.error('延遲傳送發生錯誤:', err);
-            resolve(null);
-          });
-          
-      }, 500); // 你可以在這裡調整秒數，3000 就是 3 秒
-    });
-  }
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-  // 定義 Quick Reply 按鈕 (所有回覆共用)
+  // 1. 必須「先」定義共用的 Quick Reply 按鈕，後面的程式碼才抓得到它！
   const quickReplyItems = {
     items: [
-      {
-        type: 'action',
-        action: { type: 'message', label: '📍地址', text: '地址' }
-      },
-      {
-        type: 'action',
-        action: { type: 'message', label: '⏰營業時間', text: '營業時間' }
-      },
-      {
-        type: 'action',
-        action: { type: 'message', label: '📞聯絡電話', text: '聯絡電話' }
-      },
-      /*{
-        type: 'action',
-        action: { type: 'message', label: '快速回覆', text: '更多回覆' }
-      },
-      {
-        type: 'action',
-        action: { type: 'message', label: '⭐限定', text: '限定品項' }
-      },
-      {
-        type: 'action',
-        action: { type: 'message', label: '招牌介紹', text: '招牌介紹' }
-      }*/
+      { type: 'action', action: { type: 'message', label: '📍地址', text: '地址' } },
+      { type: 'action', action: { type: 'message', label: '⏰營業時間', text: '營業時間' } },
+      { type: 'action', action: { type: 'message', label: '📞聯絡電話', text: '聯絡電話' } },
+      /*{ type: 'action', action: { type: 'message', label: '🍧菜單', text: '菜單' } },
+      { type: 'action', action: { type: 'message', label: '⭐限定', text: '限定品項' } },
+      { type: 'action', action: { type: 'message', label: '招牌介紹', text: '招牌介紹' } }*/
     ]
   };
 
-  const userText = event.message.text;
-  let replyMessage = {};
-  // ==========================================
-  //情境 1：使用者剛加入好友 (或解除封鎖)
-  // ==========================================
-    // ==========================================
-  // 情境 2：處理正常的文字訊息
-  // ==========================================
-  //if (event.type === 'message' && event.message.type === 'text') {
-
-
-  // 根據使用者的文字內容決定回覆訊息
-  switch (userText) {
-    case '地址':
-      replyMessage = [
-        {
+  // 2. 情境：使用者剛加入好友 (或解除封鎖)
+  if (event.type === 'follow') {
+    return new Promise((resolve) => {
+      // 延遲 2 秒 (2000 毫秒)
+      setTimeout(() => {
+        const replyMessage = {
           type: 'text',
-          text: '我們在這裡！'
-        },
-        {
-          type: 'location',
-          title: '璞園藝術坊',
-          address: '南投縣竹山鎮延平新村1-20號',
-          latitude: 23.765590,
-          longitude: 120.710719
-        },
-        {
+          text: '👇 請點擊下方選單獲取更多資訊', 
+          quickReply: quickReplyItems
+        };
+        
+        client.replyMessage(event.replyToken, replyMessage)
+          .then(() => resolve(null))
+          .catch((err) => {
+            console.error('延遲傳送發生錯誤:', err.originalError.response.data || err.message);
+            resolve(null);
+          });
+      }, 2000); 
+    });
+  }
+
+  // 3. 情境：處理正常的文字訊息
+  if (event.type === 'message' && event.message.type === 'text') {
+    const userText = event.message.text;
+    let replyMessage;
+
+    switch (userText) {
+      case '地址':
+        replyMessage = [
+          { type: 'text', text: '我們在這裡！' },
+          {
+            type: 'location',
+            title: '璞園藝術坊',
+            address: '南投縣竹山鎮延平新村1-20號',
+            latitude: 23.765590,
+            longitude: 120.710719
+          },
+          { type: 'text', text: '歡迎來店參訪！' }
+        ];
+        break;
+      case '聯絡電話':
+        replyMessage = {
           type: 'text',
-          text: '歡迎來店參訪！'
-        }
-      ];
-      break;
-    case '聯絡電話':
-      replyMessage = {
-        type: 'text',
-        text: '連絡電話：049-2653205\n歡迎來電預約參訪！'
-      };
-      break;
-    case '營業時間':
-      replyMessage = {
-        type: 'text',
-        text: '營業時間如上，歡迎來店參訪！'
-      };
-      break;
-    case '更多回覆':
-      replyMessage = {
-        type: 'text',
-        text: '你好！歡迎來到璞園藝術坊，請點選下方的快速回覆按鈕以獲取更多資訊！'
-      };
-      break;
-    default:
-      // 如果不是上述關鍵字，做原本的 Echo 功能 (複誦使用者說的話)
-      /*replyMessage = {
-        type: 'text',
-        text: '你好！歡迎來到璞園藝術坊，請點選下方的快速回覆按鈕以獲取更多資訊！'
-      };*/
-      break;
+          text: '連絡電話：049-2653205\n歡迎來電預約參觀！'
+        };
+        break;
+      /*case '菜單':
+        replyMessage = {
+          type: 'text',
+          text: '🍧 菜單請參考上圖'
+        };
+        break;
+      case '限定品項':
+        replyMessage = {
+          type: 'text',
+          text: '限定品項請參考上圖'
+        };
+        break;*/
+      case '營業時間':
+        replyMessage = {
+          type: 'text',
+          text: '營業時間如上所示，歡迎在營業時間內參訪！'
+        };
+        break;
+      /*case '招牌介紹':
+        replyMessage = {
+          type: 'template',
+          altText: '本季限定品項推薦 (請在手機上查看)',
+          template: {
+            type: 'carousel',
+            columns: [
+              {
+                thumbnailImageUrl: 'https://scontent.cdninstagram.com/v/t39.30808-6/594832321_1367900191800243_2775791771811882756_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=101&ig_cache_key=Mzc4MTYwNDYyMTQyNTY5NjQwMA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE2Mzl4MjA0OC5zZHIuQzMifQ%3D%3D&_nc_ohc=D_maq4b0BgUQ7kNvwEt8kij&_nc_oc=AdmqAZ97y69ceiECphlCvvxVfURys9hMkLKwEy4vZUbgIKsjOdZIRVEFNh1bNnONgyw&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.cdninstagram.com&_nc_gid=Djs69YMcSH9gzVGqOEI_IA&oh=00_AfkpOoBs6jLQlVHVFn6amBl5lf1ffaKmbXpoSojwk9GTfw&oe=6945DB68',
+                title: '綜合紫米粥',
+                text: '暖心甜品，冬季首選',
+                actions: [
+                  { type: 'uri', label: 'IG 貼文', uri: 'https://www.instagram.com/p/DR69V4NDd6Q/' },
+                  { type: 'uri', label: 'FB 粉專', uri: 'https://www.facebook.com/share/p/1FZzNAWhdd/' }
+                ]
+              },
+              {
+                thumbnailImageUrl: 'https://scontent.cdninstagram.com/v/t39.30808-6/586531028_1358221596101436_9187067188093452106_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=111&ig_cache_key=Mzc3MjAxOTkzOTU1ODE2NTA0NA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE2Mzl4MjA0OC5zZHIuQzMifQ%3D%3D&_nc_ohc=E8U0yUgWhjQQ7kNvwEsOHNW&_nc_oc=AdkAxHO17qfRyg2OWOSh7vOPRCdIGcRfSOZote4eH1Q8tBNBgCHvPK0Pq43_vIlveNM&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.cdninstagram.com&_nc_gid=Djs69YMcSH9gzVGqOEI_IA&oh=00_Afn4MW5WlQBpSG6NR6HxAOlIBjnW9Sxzj3XX3_SH8kopqA&oe=6945E234',
+                title: '奶霜抹茶雪花冰',
+                text: '濃郁抹茶搭配綿密奶霜',
+                actions: [
+                  { type: 'uri', label: 'IG 貼文', uri: 'https://www.instagram.com/p/DRY6CZnDgo0/' },
+                  { type: 'uri', label: 'FB 粉專', uri: 'https://www.facebook.com/share/p/16qeEKz3am/' }
+                ]
+              }
+            ]
+          }
+        };
+        break;
+      default:
+        replyMessage = {
+          type: 'text',
+          text: '目前還聽不懂這個指令喔 😅\n可以試著點選下方按鈕，或輸入「菜單」、「地址」等關鍵字！'
+        };
+        break;*/
+    }
+
+    if (Array.isArray(replyMessage)) {
+      replyMessage[replyMessage.length - 1].quickReply = quickReplyItems;
+    } else {
+      replyMessage.quickReply = quickReplyItems;
+    }
+
+    return client.replyMessage(event.replyToken, replyMessage);
   }
-  // 將 Quick Reply 附加到要回傳的訊息物件中
-  // 判斷 replyMessage 是陣列還是單一物件，將 Quick Reply 綁定在最後一則訊息
-  if (Array.isArray(replyMessage)) {
-    replyMessage[replyMessage.length - 1].quickReply = quickReplyItems;
-  } else {
-    replyMessage.quickReply = quickReplyItems;
-  }
-  // 使用 reply API 回傳
-  return client.replyMessage(event.replyToken, replyMessage);
+
+  return Promise.resolve(null);
 }
 
 // listen on port
